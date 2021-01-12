6570502b0ab7bdb7532101f7679e5d6c894da8d0
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2021-01-12 10:48:22 +0000

    additional tests including CLI

--- liquidtime/test_liquidtime.py
+++ liquidtime/test_liquidtime.py
@@ -2,10 +2,24 @@
 
 import os
 import liquidtime as lq
+from click.testing import CliRunner
+
 
 token = os.environ['LP_TOKEN']
 workspace_id = os.environ['LP_WORKSPACE_ID']
+member_id = os.environ['LP_MEMBER_ID']
+
+
+def test_cli():
+    runner = CliRunner()
+    result = runner.invoke(lq.cli)
+    assert result.exit_code == 0
+    # assert result.output == '(help documentation)'
 
 
 def test_headers():
     assert lq.headers(token) == {'Authorization': f"Bearer {token}"}
+
+
+def test_get_member_id():
+    assert lq.get_member_id(token) == member_id
